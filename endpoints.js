f53f91141b3aa44eb77576cee8c9ff2144b5079d
D'Santi <35488718+dsantix@users.noreply.github.com>
2021-11-06 00:42:59 -0300

    inital commit

---
+++ endpoints.js
@@ -0,0 +1,28 @@
+{
+  "name": "csgo-api-esports",
+  "version": "1.0.0",
+  "main": "index.js",
+  "license": "MIT",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "hltv-api": "^3.0.0",
+    "endpoints": "^file:./endpoints"
+  },
+  "author": "dsanti",
+  "description": "csgo-api",
+  "devDependencies": {},
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/dsantix/csgo-api-esports.git"
+  },
+  "keywords": [
+    "csgo"
+  ],
+  "bugs": {
+    "url": "https://github.com/dsantix/csgo-api-esports/issues"
+  },
+  "homepage": "https://github.com/dsantix/csgo-api-esports#readme"
+}
